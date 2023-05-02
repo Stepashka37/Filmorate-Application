a91fa6a40bf93e1aa3da332ad469c6d6378b97f3
Antroverden <Antroverden@yandex.ru>
2023-05-02 10:56:13 +0300

    fix: исправить замечания

--- src/main/java/ru/yandex/practicum/filmorate/module/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/module/Review.java
@@ -28,6 +28,10 @@ public class Review {
 
     private int useful;
 
+    public Review() {
+        useful = 0;
+    }
+
     public Review(Integer reviewId, String content, boolean isPositive, Integer userId, Integer filmId, Integer useful) {
         this.reviewId = reviewId;
         this.content = content;
