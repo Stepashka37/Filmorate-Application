98329927c7aef731e237738ac6a7ef8d23bc2983
N.Koriukova <60816736+Salaia@users.noreply.github.com>
2023-05-11 19:30:24 +0300

    Fix recommendations, code style (#32)
    * fix: recommendations
    
    * fix: recommendations, codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
@@ -27,6 +27,10 @@ import java.util.List;
 @Slf4j
 public class FilmDbStorage implements FilmsStorage {
     private final JdbcTemplate jdbcTemplate;
+    private final static int MOST_NEGATIVE_SCORE = 1;
+    private final static int LEAST_NEGATVE_SCORE = 5;
+    private final static int LEAST_POSITIVE_SCORE = 6;
+    private final static int MOST_POSITIVE_SCORE = 10;
 
     @Autowired
     public FilmDbStorage(JdbcTemplate jdbcTemplate) {
@@ -188,23 +192,26 @@ public class FilmDbStorage implements FilmsStorage {
 
     @Override
     public List<Film> recommendFilms(Integer userId) {
-        String findFilms = "SELECT *" +
-                " FROM FILM AS f " +
-                " WHERE f.FILM_ID IN (SELECT DISTINCT sc.FILM_ID" +
-                " FROM FILM_SCORES AS sc" +
-                " JOIN (SELECT fs.USER_ID AS other_user_id" +
-                " FROM FILM_SCORES AS fs " +
-                " JOIN (select fs.film_id as film_id, fs.score as aim_score" +
-                "                 from FILM_SCORES as fs " +
-                "                 where fs.user_id = " + userId + ") as aus ON fs.FILM_ID = aus.film_id" +
-                " WHERE fs.USER_ID != " + userId  +
-                " AND ( " +
-                " (fs.SCORE BETWEEN 1 AND 5 AND aus.aim_score BETWEEN 1 AND 5)" +
-                " OR (fs.SCORE BETWEEN 6 AND 10 AND aus.aim_score BETWEEN 6 AND 10)" +
+
+        String aimUserReviews = "(SELECT fs.film_id FROM FILM_SCORES as fs WHERE fs.user_id = " + userId + ")";
+        String aimUserScores = " (select fs.film_id as film_id, fs.score as aim_score " +
+                " from FILM_SCORES as fs " +
+                " where fs.user_id = " + userId + ") as aus ";
+        String filters = " WHERE fs.USER_ID != " + userId + " AND (" +
+                " (fs.SCORE >= " + MOST_NEGATIVE_SCORE + " AND fs.SCORE <= " + LEAST_NEGATVE_SCORE +
+                " AND aus.aim_score >= " + MOST_NEGATIVE_SCORE + " AND aus.aim_score <= " + LEAST_NEGATVE_SCORE + ")" +
+                " OR (fs.SCORE >= " + LEAST_POSITIVE_SCORE + " AND fs.SCORE <= " + MOST_POSITIVE_SCORE +
+                " AND aus.aim_score >= " + LEAST_POSITIVE_SCORE + " AND aus.aim_score <= " + MOST_POSITIVE_SCORE + ")" +
                 " ) GROUP BY fs.USER_ID) AS ur ON ur.other_user_id = sc.USER_ID" +
-                " WHERE sc.FILM_ID NOT IN (SELECT fs.film_id FROM FILM_SCORES as fs WHERE fs.user_id = " + userId + ")" +
-                " AND sc.score BETWEEN 6 AND 10);";
-        return jdbcTemplate.query(findFilms, (rs, rowNum) -> makeFilm(rs));
+                " WHERE sc.FILM_ID NOT IN " + aimUserReviews +
+                " AND sc.score >= " + LEAST_POSITIVE_SCORE + " AND sc.score <= " + MOST_POSITIVE_SCORE + ");";
+        String recommendUsers = "(SELECT fs.USER_ID AS other_user_id FROM FILM_SCORES AS fs " +
+                " JOIN " + aimUserScores + " ON fs.FILM_ID = aus.film_id" + filters;
+        String findFilmsIds = "(SELECT DISTINCT sc.FILM_ID FROM FILM_SCORES AS sc JOIN " + recommendUsers;
+        String makeFilms = "SELECT * FROM FILM AS f " +
+                " WHERE f.FILM_ID IN" + findFilmsIds;
+        return jdbcTemplate.query(makeFilms, (rs, rowNum) -> makeFilm(rs));
+
     }
 
     private Film makeFilm(ResultSet rs) throws SQLException {
