6662ff583e3e9e09e59380afab547d6909a7bb7a
Antroverden <115946141+Antroverden@users.noreply.github.com>
2023-05-11 19:30:38 +0300

    Refactor rating (#33)
    * feature: добавить оценки от 1 до 10 вместо лайков и вычисление рейтинга как среднего арифметического
    
    * feature: добавить оценки от 1 до 10 вместо лайков и вычисление рейтинга как среднего арифметического
    
    * feature: добавить оценки от 1 до 10 вместо лайков и вычисление рейтинга как среднего арифметического
    
    * fix: небольшие правки по валидации поставленного рейтинга
    
    * fix:  добавить тест с некорректным значением score (меньше 1 или больше 10)
    
    * fix:  добавить тест с некорректным значением score (меньше 1 или больше 10)
    
    * fix:  добавить тест с некорректным значением score (меньше 1 или больше 10)

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -415,7 +415,7 @@ class FilmControllerTest {
 
     @SneakyThrows
     @Test
-    public void likeFilm() {
+    public void scoreFilm() {
         Film film = Film.builder().name("Name")
                 .description("Film description")
                 .releaseDate(LocalDate.of(2016, 03, 04))
@@ -471,17 +471,67 @@ class FilmControllerTest {
                 .mpa(new Rating(1, "PG"))
                 .genres(new HashSet<>())
                 .build();
+        User user1 = User.builder()
+                .email("yandex1@yandex.ru")
+                .login("user1")
+                .name("user1")
+                .birthday(LocalDate.of(1997, 06, 03))
+                .friends(new HashSet<>())
+                .build();
+        String userGson = objectMapper.writeValueAsString(user1);
+        mockMvc.perform(post("/users")
+                .contentType("application/json")
+                .content(userGson)
+        ).andExpect(status().isCreated());
         String gson1 = objectMapper.writeValueAsString(film);
         mockMvc.perform(post("/films")
                 .contentType("application/json")
                 .content(gson1)
         ).andExpect(status().isCreated());
 
-        mockMvc.perform(put("/films/10/like/1")
+        mockMvc.perform(put("/films/10/score/1?score=1")
                 .contentType("application/json")
                 .content(gson1)
         ).andExpect(status().isNotFound());
+    }
 
+    @SneakyThrows
+    @Test
+    public void wrongScoreFilm() {
+        Film film = Film.builder().name("Name")
+                .description("Film description")
+                .releaseDate(LocalDate.of(2016, 3, 4))
+                .duration(120)
+                .mpa(new Rating(1, "PG"))
+                .genres(new HashSet<>())
+                .build();
+        User user1 = User.builder()
+                .email("yandex1@yandex.ru")
+                .login("user1")
+                .name("user1")
+                .birthday(LocalDate.of(1997, 06, 03))
+                .friends(new HashSet<>())
+                .build();
+        String userGson = objectMapper.writeValueAsString(user1);
+        mockMvc.perform(post("/users")
+                .contentType("application/json")
+                .content(userGson)
+        ).andExpect(status().isCreated());
+        String gson1 = objectMapper.writeValueAsString(film);
+        mockMvc.perform(post("/films")
+                .contentType("application/json")
+                .content(gson1)
+        ).andExpect(status().isCreated());
+
+        mockMvc.perform(put("/films/1/score/1?score=0")
+                .contentType("application/json")
+                .content(gson1)
+        ).andExpect(status().isBadRequest());
+
+        mockMvc.perform(put("/films/1/score/1?score=11")
+                .contentType("application/json")
+                .content(gson1)
+        ).andExpect(status().isBadRequest());
     }
 
     @SneakyThrows
@@ -551,7 +601,7 @@ class FilmControllerTest {
 
     @SneakyThrows
     @Test
-    public void removeLikeWithInvalidId() {
+    public void removeScoreWithInvalidId() {
         Film film = Film.builder().name("Name")
                 .description("Film description")
                 .releaseDate(LocalDate.of(2016, 03, 04))
@@ -687,7 +737,7 @@ class FilmControllerTest {
         });
 
         assertEquals(1, films.size());
-        assertEquals(1, films.get(0).getId());
+        assertEquals(2, films.get(0).getId());
 
         result = mockMvc.perform(get("/films/popular")
                         .contentType("application/json")
@@ -700,8 +750,8 @@ class FilmControllerTest {
         });
 
         assertEquals(2, films.size());
-        assertEquals(1, films.get(0).getId());
-        assertEquals(2, films.get(1).getId());
+        assertEquals(2, films.get(0).getId());
+        assertEquals(1, films.get(1).getId());
 
         mockMvc.perform(get("/films/popular?count=-1")
                 .contentType("application/json")
